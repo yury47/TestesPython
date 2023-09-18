ace1832866cc7f80ed27a84b85e33eb348d80cf4
Francisco Yury Pinheiro Silva <66030421+yury47@users.noreply.github.com>
2023-09-18 13:00:02 -0300

    Update TesteGitHub1.py

--- TesteGitHub1.py
+++ TesteGitHub1.py
@@ -1,3 +1,10 @@
-print('Hello world')
-a = int(input())
-print('Hello world',a)
+x = input().split()
+a, b, c = x
+a = int(a)
+b = int(b)
+c = int(c)
+d = a + b
+if (d == c):
+    print("true")
+else:
+    print("False")
